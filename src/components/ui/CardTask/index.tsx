5d79439374df8575aa04276518d5aa60cd8c7908
Anna <ana84168@gmail.com>
2023-02-21 23:16:59 -0500

    feat: add labels badge

--- src/components/ui/CardTask/index.tsx
+++ src/components/ui/CardTask/index.tsx
@@ -2,6 +2,7 @@ import {
   ActionIcon,
   Avatar,
   Badge,
+  createStyles,
   Group,
   Paper,
   Stack,
@@ -10,9 +11,26 @@ import {
 } from "@mantine/core";
 import { Task } from "modules/app/datatypes";
 import { useState } from "react";
+import { LabelColor } from "../Task/label";
+import { GenericLeadTaskMenu } from "../Task/lead";
 
 import { GenericPriorityMenu, PriorityIcon } from "../Task/priority";
 import { GenericStatusMenu, StatusIcon } from "../Task/status";
+
+const useStyles = createStyles(theme => ({
+  badge: {
+    [theme.fn.smallerThan(375)]: {
+      width: "65px",
+      overflow: "hidden",
+      textOverflow: "ellipsis",
+    },
+    [theme.fn.smallerThan(330)]: {
+      width: "40px",
+      overflow: "hidden",
+      textOverflow: "ellipsis",
+    },
+  },
+}));
 
 type DndTaskListElementProps = {
   task: Task;
@@ -26,6 +44,7 @@ export const DndTaskListElement = ({
   checked = false,
 }: DndTaskListElementProps) => {
   const smallDate = new Date(task.createdAt).toDateString().split(" ").slice(1, 3).join(" ");
+  const { classes } = useStyles();
   const theme = useMantineTheme();
 
   // console.log(task.assigneeId);
@@ -36,9 +55,13 @@ export const DndTaskListElement = ({
           <Text size={"sm"} color={"dimmed"}>
             MIN-169
           </Text>
-          <Avatar size="sm" radius="xl">
-            {/* {task.assigneeId} */}
-          </Avatar>
+          <GenericLeadTaskMenu task={task}>
+            <ActionIcon variant="transparent">
+              <Avatar size="sm" radius="xl">
+                {/* {task.leadId} */}
+              </Avatar>
+            </ActionIcon>
+          </GenericLeadTaskMenu>
         </Group>
         <Text size={"sm"}>{task.title}</Text>
         <Group spacing={8}>
@@ -52,7 +75,7 @@ export const DndTaskListElement = ({
               {StatusIcon(theme, task.status)}
             </ActionIcon>
           </GenericStatusMenu>
-          <Badge>MINSKY{/* {task.projectId} */}</Badge>
+          {task.project && <Badge className={classes.badge}>{task.project?.name}</Badge>}
         </Group>
       </Stack>
     </Paper>
