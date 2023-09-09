8da95888873f140714dc7ead7799e49bb0544e94
Anna <ana84168@gmail.com>
2023-09-08 20:05:34 -0500

    debug: login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,39 +1,25 @@
-import {
-  Button,
-  Center,
-  Divider,
-  Paper,
-  Stack,
-  useMantineColorScheme,
-  useMantineTheme,
-} from "@mantine/core";
+import { Button, Center, Stack } from "@mantine/core";
 import { BrandGithub } from "tabler-icons-react";
 
-import PlexoLogo from "components/resources/PlexoLogo";
-
 const LoginPage = () => {
-  const { colorScheme } = useMantineColorScheme();
-  const theme = useMantineTheme();
-
   return (
     <Center style={{ height: "100vh" }}>
-      <Stack w={420} miw={200} align={"center"} m={20}>
-        <PlexoLogo typographyColor={colorScheme === "light" ? theme.colors.gray[9] : undefined} />
-        <Paper w={"100%"} radius="md" p="xl" withBorder>
-          <Stack>
-            <Button onClick={() => console.log("click")}>Login</Button>
-            <Divider label="Or, continue with" labelPosition="center" />
-
-            <Button
-              component="a"
-              href={process.env.NEXT_PUBLIC_URL_AUTH || "/auth/github"}
-              leftIcon={<BrandGithub />}
-              color="dark"
-            >
-              Login with Github
-            </Button>
-          </Stack>
-        </Paper>
+      <Stack>
+        <Button
+          onClick={() => {
+            console.log("clic");
+          }}
+        >
+          Login
+        </Button>
+        <Button
+          component="a"
+          href={process.env.NEXT_PUBLIC_URL_AUTH || "/auth/github"}
+          leftIcon={<BrandGithub />}
+          color="dark"
+        >
+          Login
+        </Button>
       </Stack>
     </Center>
   );
