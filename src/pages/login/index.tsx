b9772c9447bdc4b56dc0cb98190ba3b83f946051
Bregy Malpartida <bregy@minsky.cc>
2023-09-10 22:13:06 -0500

    fix: change minimus size for password

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -42,7 +42,7 @@ const LoginPage = () => {
 
     validate: {
       email: val => (/^\S+@\S+$/.test(val) ? null : "Invalid email"),
-      password: val => (val.length <= 6 ? "Password should include at least 6 characters" : null),
+      password: val => (val.length <= 5 ? "Password should include at least 5 characters" : null),
     },
   });
 
